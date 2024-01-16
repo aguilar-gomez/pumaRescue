a8774660d3d878b764515d19c08f16bfc3f00f53
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2024-01-16 10:49:01 -0800

    Update runHet.sh

--- PyagourundiRef/Heterozygosity/runHet.sh
+++ PyagourundiRef/Heterozygosity/runHet.sh
@@ -1,15 +1,11 @@
+VCF=unfiltered_all2yag.vcf.gz
 
-module load htslib
-module load vcftools
-
-#Quick vcftools genome-wide estimate
-VCF=bricei_simplePASS_variants_chr.vcf.gz
-vcftools --gzvcf $VCF --het --out genome-wide.hetPlink
-
-#Use Jacqueline's vaquita script
 tabix -p vcf $VCF
- awk -v OFS="\t" '{print "chr" FNR,$3}' autosomes > chromosome_lengths.txt
 
-#Run script 
-#Modify line to make it work on python3:
-python3 ./SlidingWindowHet.py bricei_simplePASS_all_$chr.vcf.gz chromosome_lengths.txt 1000000 100000 $chr 
+while read scaffold len
+do
+echo $scaffold
+bcftools view -r $scaffold -Oz -o pumaUnfil_${scaffold}.vcf.gz $VCF
+tabix -p puma_${scaffold}.vcf.gz 
+python3 ./SlidingWindowHet.py pumaUnfil_${scaffold}.vcf.gz scaffolds_10Mb 1000000 100000 $scaffold
+done <scaffolds_10Mb
