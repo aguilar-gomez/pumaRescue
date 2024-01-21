b38b27e92d6f73db79340a72cfa53b4b739f0f4c
Lin Yuan <52481311+yuanlin0710@users.noreply.github.com>
2024-01-21 21:34:15 +0800

    Update download SRA from NCBI.sh

--- ProcessReads/download SRA from NCBI.sh
+++ ProcessReads/download SRA from NCBI.sh
@@ -1,2 +1,3 @@
 nohup fasterq-dump  --split-files SRR6071635 > out.download2
 nohup bwa index GCF_014898765.1_PumYag_genomic.fna > out.index &
+nohup perl /home/diana/bin/countfasta.pl GCF_014898765.1_PumYag_genomic.fna > out.countfasta_outgroup &
