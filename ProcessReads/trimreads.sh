2c5b0a9136d62bf959731675714de7bc12eaed69
Lin Yuan <52481311+yuanlin0710@users.noreply.github.com>
2024-01-21 21:33:36 +0800

    Update trimreads.sh

--- ProcessReads/trimreads.sh
+++ ProcessReads/trimreads.sh
@@ -1,10 +1,15 @@
-for sample in *SRR7690239*
+for sample in *fastq.gz*
 do
 name=${sample%_*1*}
 echo $name $sample ${name}_2.fastq.gz
-java -jar ~/bin/trimmomatic-0.39.jar PE -threads 1 $sample ${name}_2.fastq.gz $n
-ame.trim.R1.fq.gz output_forward_unpaired.fq.gz $name.trim.R2.fq.gz output_rever
-se_unpaired.fq.gz ILLUMINACLIP:NexteraAdapters.fa:2:30:10:2:keepBothReads LEADIN
-G:3 TRAILING:3 MINLEN:75
+java -jar ~/bin/trimmomatic-0.39.jar PE -threads 1 $sample ${name}_2.fastq.gz $name.trim.R1.fq.gz output_forward_unpaired.fq.gz $name.trim.R2.fq.gz output_reverse_unpaired.fq.gz ILLUMINACLIP:NexteraAdapters.fa:2:30:10:2:keepBothReads LEADING:3 TRAILING:3 MINLEN:75
 rm *unpaired*
+done
+
+
+for sample in SRR13887964.trim.R1.fq
+do
+name=${sample%trim*1*}
+echo $name $sample ${name}trim.R2.fq
+perl /home/diana/bin/prinseq-lite.pl -fastq2 $sample ${name}trim.R2.fq -custom_params “AACCCT 5; G 15” -stats_all -out_good 
 done
