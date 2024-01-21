e8f367899014319b7dea69ee6a7ef423ae46220c
Lin Yuan <52481311+yuanlin0710@users.noreply.github.com>
2024-01-21 21:31:23 +0800

    Update download SRA from NCBI.sh

--- ProcessReads/download SRA from NCBI.sh
+++ ProcessReads/download SRA from NCBI.sh
@@ -1,4 +1,2 @@
-for accession in SRR7537344 SRR7537345
-do
-fasterq-dump  --split-files $accession &
-done
+nohup fasterq-dump  --split-files SRR6071635 > out.download2
+nohup bwa index GCF_014898765.1_PumYag_genomic.fna > out.index &
