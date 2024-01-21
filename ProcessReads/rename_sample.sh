cc7f9491d2ff78c6e4ac7c7f0ce54d079664013e
Lin Yuan <52481311+yuanlin0710@users.noreply.github.com>
2024-01-21 21:41:57 +0800

    Create rename_sample.sh

---
+++ ProcessReads/rename_sample.sh
@@ -0,0 +1,112 @@
+# doing some re-naming stuff to make everything clear
+
+mv RNDAG02-A_FP228.yag.bam AFP3.yag.bam
+mv RNDAG02-B_FP231.yag.bam AFP4.yag.bam
+mv RNDAG02-C_FP245.yag.bam AFP5.yag.bam
+mv RNDAG02-D_UCFP160.yag.bam AFP6.yag.bam
+mv RNDAG02-E_UCFP180.yag.bam AFP7.yag.bam
+mv RNDAG02-F_UCFP191.yag.bam AFP8.yag.bam
+mv RNDAG02-G_UCFP194.yag.bam AFP9.yag.bam
+mv RNDAG02-H_UCFP217.yag.bam AFP10.yag.bam
+mv RNDAG02-I_UCFP223.yag.bam AFP11.yag.bam
+mv RNDAG02-J_UCFP225.yag.bam AFP12.yag.bam
+mv RNDAG02-K_UCFP226.yag.bam AFP13.yag.bam
+mv RNDAG02-L_UCFP229.yag.bam AFP14.yag.bam
+mv RNDAG02-M_UCFP243.yag.bam AFP15.yag.bam
+mv RNDAG02-N_UCFP244.yag.bam AFP16.yag.bam
+mv RNDAG02-O_UCFP251.yag.bam AFP17.yag.bam
+mv RNDAG02-P_UCFP263.yag.bam AFP18.yag.bam
+mv RNDAG02-Q_UCFP271.yag.bam AFP19.yag.bam
+mv RNDAG02-R_UCFP274.yag.bam AFP20.yag.bam
+mv RNDAG02-S_UCFP275.yag.bam AFP21.yag.bam
+mv RNDAG02-T_UCFP279.yag.bam AFP22.yag.bam
+mv RNDAG02-U_UCFP281.yag.bam AFP23.yag.bam
+mv RNDAG02-V_UCFP282.yag.bam AFP24.yag.bam
+mv RNDAG02-W_UCFP286.yag.bam AFP25.yag.bam
+mv RNDAG02-X_UCFP287.yag.bam AFP26.yag.bam
+mv RNDAG02-Y_UCFP289.yag.bam AFP27.yag.bam
+mv RNDAG02-Z_UCFP294.yag.bam AFP28.yag.bam
+mv RNDAG02-AA_UCFP300.yag.bam AFP29.yag.bam
+mv RNDAG02-AB_UCFP304.yag.bam AFP30.yag.bam
+mv RNDAG02-AC_UCFP305.yag.bam AFP31.yag.bam
+
+
+mv RNDAG02-A_FP228.yag.bam.bai AFP3.yag.bam.bai
+mv RNDAG02-B_FP231.yag.bam.bai AFP4.yag.bam.bai
+mv RNDAG02-C_FP245.yag.bam.bai AFP5.yag.bam.bai
+mv RNDAG02-D_UCFP160.yag.bam.bai AFP6.yag.bam.bai
+mv RNDAG02-E_UCFP180.yag.bam.bai AFP7.yag.bam.bai
+mv RNDAG02-F_UCFP191.yag.bam.bai AFP8.yag.bam.bai
+mv RNDAG02-G_UCFP194.yag.bam.bai AFP9.yag.bam.bai
+mv RNDAG02-H_UCFP217.yag.bam.bai AFP10.yag.bam.bai
+mv RNDAG02-I_UCFP223.yag.bam.bai AFP11.yag.bam.bai
+mv RNDAG02-J_UCFP225.yag.bam.bai AFP12.yag.bam.bai
+mv RNDAG02-K_UCFP226.yag.bam.bai AFP13.yag.bam.bai
+mv RNDAG02-L_UCFP229.yag.bam.bai AFP14.yag.bam.bai
+mv RNDAG02-M_UCFP243.yag.bam.bai AFP15.yag.bam.bai
+mv RNDAG02-N_UCFP244.yag.bam.bai AFP16.yag.bam.bai
+mv RNDAG02-O_UCFP251.yag.bam.bai AFP17.yag.bam.bai
+mv RNDAG02-P_UCFP263.yag.bam.bai AFP18.yag.bam.bai
+mv RNDAG02-Q_UCFP271.yag.bam.bai AFP19.yag.bam.bai
+mv RNDAG02-R_UCFP274.yag.bam.bai AFP20.yag.bam.bai
+mv RNDAG02-S_UCFP275.yag.bam.bai AFP21.yag.bam.bai
+mv RNDAG02-T_UCFP279.yag.bam.bai AFP22.yag.bam.bai
+mv RNDAG02-U_UCFP281.yag.bam.bai AFP23.yag.bam.bai
+mv RNDAG02-V_UCFP282.yag.bam.bai AFP24.yag.bam.bai
+mv RNDAG02-W_UCFP286.yag.bam.bai AFP25.yag.bam.bai
+mv RNDAG02-X_UCFP287.yag.bam.bai AFP26.yag.bam.bai
+mv RNDAG02-Y_UCFP289.yag.bam.bai AFP27.yag.bam.bai
+mv RNDAG02-Z_UCFP294.yag.bam.bai AFP28.yag.bam.bai
+mv RNDAG02-AA_UCFP300.yag.bam.bai AFP29.yag.bam.bai
+mv RNDAG02-AB_UCFP304.yag.bam.bai AFP30.yag.bam.bai
+mv RNDAG02-AC_UCFP305.yag.bam.bai AFP31.yag.bam.bai
+
+
+
+
+mv SRR6467070.bam TX108.yag.bam
+mv SRR6467071.bam TX106.yag.bam
+mv SRR6467072.bam TX107.yag.bam
+mv SRR6467073.bam AFP1.yag.bam
+mv SRR6467074.bam AFP2.yag.bam
+mv SRR6467075.bam TX101.yag.bam
+mv SRR6467076.bam TX105.yag.bam
+mv SRR6467079.bam CYP45.yag.bam
+mv SRR6467080.bam CYP60.yag.bam
+
+
+mv SRR6467070.bam.bai TX108.yag.bam.bai
+mv SRR6467071.bam.bai TX106.yag.bam.bai
+mv SRR6467072.bam.bai TX107.yag.bam.bai
+mv SRR6467073.bam.bai AFP1.yag.bam.bai
+mv SRR6467074.bam.bai AFP2.yag.bam.bai
+mv SRR6467075.bam.bai TX101.yag.bam.bai
+mv SRR6467076.bam.bai TX105.yag.bam.bai
+mv SRR6467079.bam.bai CYP45.yag.bam.bai
+mv SRR6467080.bam.bai CYP60.yag.bam.bai
+
+
+mv BR338.bam BR338.yag.bam
+mv BR338.bam.bai BR338.yag.bam.bai
+mv BR406.bam BR406.yag.bam
+mv BR406.bam.bai BR406.yag.bam.bai
+mv CYP47.bam CYP47.yag.bam
+mv CYP47.bam.bai CYP47.yag.bam.bai
+mv CYP51.bam CYP51.yag.bam
+mv CYP51.bam.bai CYP51.yag.bam.bai
+mv EVG16.bam EVG16.yag.bam
+mv EVG16.bam.bai EVG16.yag.bam.bai
+mv EVG21.bam EVG21.yag.bam
+mv EVG21.bam.bai EVG21.yag.bam.bai
+mv SC29.bam SC29.yag.bam
+mv SC29.bam.bai SC29.yag.bam.bai
+mv SC36new.bam SC36new.yag.bam
+mv SC36new.bam.bai SC36new.yag.bam.bai
+mv SMM12.bam SMM12.yag.bam
+mv SMM12.bam.bai SMM12.yag.bam.bai
+mv SMM13.bam SMM13.yag.bam
+mv SMM13.bam.bai SMM13.yag.bam.bai
+mv SMM22.bam SMM22.yag.bam
+mv SMM22.bam.bai SMM22.yag.bam.bai
+mv YNP198.bam YNP198.yag.bam
+mv YNP198.bam.bai YNP198.yag.bam.bai
