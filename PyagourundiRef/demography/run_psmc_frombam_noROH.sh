33f784cd5a0cdacb08553b9d11f63baaed20090c
Lin Yuan <52481311+yuanlin0710@users.noreply.github.com>
2024-03-18 22:03:15 +0800

    Update run_psmc_frombam_noROH.sh
    try install old version samtools (1.9) that still keeps -u/-v options

--- PyagourundiRef/demography/run_psmc_frombam_noROH.sh
+++ PyagourundiRef/demography/run_psmc_frombam_noROH.sh
@@ -30,13 +30,15 @@ wait
 # our code
 # with ROH, no bootstrap
 # command example: nohup sh psmc.sh TX101.yag.bam 11 NONE > out.pmsc_TX101 &
+conda create -n samtools1.9
+conda install -c bioconda samtools=1.9
 
 out=$(basename $bamfile .bam)
-{ time bcftools mpileup -C50 -O u -f GCF_014898765.1_PumYag_genomic.fna $bamfile 2> fq/${out}_stderr.txt  | bcftools call -c - | vcfutils.pl vcf2fq -d $mindep -D $maxdep | gzip > fq/${out}withROH.fq.gz ; } 2> fq/bam2fq_${out}withROH.time 
+{ time samtools mpileup -C50 -u -v -f GCF_014898765.1_PumYag_genomic.fna $bamfile 2> fq/${out}_stderr.txt  | bcftools call -c - | vcfutils.pl vcf2fq -d $mindep -D $maxdep | gzip > fq/${out}withROH.fq.gz ; } 2> fq/bam2fq_${out}withROH.time 
 echo "Waiting for bam2fq to finish"
 wait
 echo "Running fq2psmcfa"
-{ time fq2psmcfa -q 30 -s 100 fq/${out}withROH.fq.gz > psmcIN/${out}withROH.psmcfa ; } 2> psmcIN/fq2psmc_${out}withROH.time
+{ time /home/lin.yuan/programs/psmc-master/utils/fq2psmcfa -q 30 -s 100 fq/${out}withROH.fq.gz > psmcIN/${out}withROH.psmcfa ; } 2> psmcIN/fq2psmc_${out}withROH.time
 { time psmc -N25 -t15 -r5 -p "4+25*2+4+6" -o psmcOUT/${out}withROH.psmc psmcIN/${out}withROH.psmcfa ; } 2> psmcOUT/psmc_${out}withROH.time
 wait
 
