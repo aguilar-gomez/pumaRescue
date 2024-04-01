704103e1ad2d6a314aef42cb2ad2fbf94c9626ae
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2024-04-01 11:42:46 -0700

    Update conda_enviroment.sh

--- PyagourundiRef/preprocessing/conda_enviroment.sh
+++ PyagourundiRef/preprocessing/conda_enviroment.sh
@@ -1,4 +1,12 @@
+
+#GATK
 conda create -n gatk_env
 conda activate gatk_env
 conda install -c bioconda gatk4=4.2.2
+
+#RepeatMasker
+conda create -n repeatmask
+conda activate repeatmask
+conda install -c bioconda -c conda-forge repeatmasker
+
 
