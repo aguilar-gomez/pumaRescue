b7c726ea34a36d5b35c841d86dd652d337bbe17e
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2024-04-01 12:41:30 -0700

    Update RepeatMasking.sh

--- PyagourundiRef/preprocessing/RepeatMasking.sh
+++ PyagourundiRef/preprocessing/RepeatMasking.sh
@@ -29,17 +29,34 @@ RepeatMasker -engine ncbi -s -align -species "mammalia" -dir PyagRep $FASTA
 #Run with specific species
 RepeatMasker -engine ncbi -s -align -species "jaguarundi" -dir PyagRepeatMasked $NAME.reduced.fasta 
 
+### RepeatMask VCF
+#!/bin/bash
+#$ -cwd
+#$ -j y
+#$ -o RM.log.$JOB_ID.$TASK_ID
+#$ -l highp,h_rt=72:00:00,h_data=24G
+## and the number of cores as needed:
+#$ -pe shared 1
+#$ -M daguilar
+#$ -t 1-237:1
+
+. /u/local/Modules/default/init/modules.sh
+
+module load gatk/4.2.0.0
+module load htslib
+
+REFERENCE=~/project-kirk-bigdata/Pconcolor/genome_outgroup/GCF_014898765.1_PumYag_genomic.fna
+IDX=$(printf %03d ${SGE_TASK_ID})
+REGION=$(ls $(dirname ${REFERENCE})/intervals/*_${IDX}.bed)
 
 #Regions to exclude:
-export SOFTMASK=~/project-kirk-bigdata/Pconcolor/genome_outgroup/Masks/GCF_014898765.1_PumYag_genomic.SoftMask.bed
+export REPEATMASK=~/project-kirk-bigdata/Pconcolor/genome_outgroup/Masks/GCF_014898765.1_PumYag_genomic.SoftMask.bed
 ### VariantFiltration to mask repeats
 # Note: GATK requires indexed mask file
-gatk IndexFeatureFile -I ${SOFTMASK}
-#gatk IndexFeatureFile 
+#gatk IndexFeatureFile -I ${REPEATMASK}
 
-IDX=$(printf %03d ${SGE_TASK_ID})
-VCF=bricei_joint_${IDX}_snpEff_SIFT.vcf.gz  
-REGION=$(ls $(dirname ${REFERENCE})/intervals/*_${IDX}.bed)
+VCF=puma_allsamples_${IDX}_snpEff_filter.vcf.gz
+tabix -p vcf $VCF
 
 gatk VariantFiltration \
 -R ${REFERENCE} \
