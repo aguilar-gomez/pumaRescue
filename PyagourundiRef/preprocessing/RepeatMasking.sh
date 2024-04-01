688e5583afb30ad8e07d84d006bb6e5b5ff82d90
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2024-04-01 11:40:18 -0700

    Update RepeatMasking.sh

--- PyagourundiRef/preprocessing/RepeatMasking.sh
+++ PyagourundiRef/preprocessing/RepeatMasking.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Repeat masking
 # Define variables:
-export FASTA=
+export FASTA=GCF_014898765.1_PumYag_genomic.fna
 export SPECIES="Pyagouaroundi"
 export NAME=${FASTA%.fa*}
 
@@ -11,13 +11,18 @@ module load anaconda3
 #conda create -n repeatmask
 conda activate repeatmask
 #conda install -c bioconda -c conda-forge repeatmasker
+#conda install -c bioconda phrap
 
 #Tutorial:
 #https://darencard.net/blog/2022-07-09-genome-repeat-annotation/
 
 #/u/home/d/daguilar/.conda/envs/repeatmask/bin/RepeatMasker - 4.1.5
-RepeatMasker -engine crossmatch -s -align -species "${SPECIES}" -dir $NAME $FASTA
+#Mammalia runs well
+RepeatMasker -engine ncbi -s -align -species "mammalia" -dir PyagRep $FASTA 
 
+#Test runs well
+RepeatMasker -engine ncbi -s -align -species "carnivora" -dir PyagRep $FASTA 
+RepeatMasker -engine ncbi -s -align -species "felidae" -dir PyagRepFelidae $FASTA 
 
 
 
