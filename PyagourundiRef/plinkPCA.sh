f0e1109d429594b725657893c4075731afbc7afb
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2024-04-03 13:42:07 -0700

    Update plinkPCA.sh

--- PyagourundiRef/plinkPCA.sh
+++ PyagourundiRef/plinkPCA.sh
@@ -1,8 +1,19 @@
+qrsh -l h_rt=4:00:00,h_data=40G -pe shared 2
+
 VCF=puma_simplePASS_variants_all.vcf.gz
 cp -s ~/project-klohmuel/Pconcolor/vcfs/PASS8/$VCF .
+
 module load plink
 #PLINK v1.90b6.24
+
+
+#All variants no Maf
 plink --vcf $VCF--make-bed --out puma_variants \
                 --threads 15 --const-fid 0 --set-missing-var-ids @:#\$1,\$2 --snps-only \
                 --biallelic-only --allow-extra-chr --vcf-half-call r
 
+#MAF filter and PCA
+OUTPUT_NAME=puma_variantsmaf1
+plink --vcf $VCF --make-bed --out $OUTPUT_NAME --threads 16 --const-fid 0 \
+                   --set-missing-var-ids @:#\$1,\$2 --snps-only --biallelic-only \
+                   --allow-extra-chr --maf 0.01 --geno 0.0 --pca
