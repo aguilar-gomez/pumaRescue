d2146411efee3c1438b7fdb0c6a7c4de14dc7e08
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2024-04-03 22:00:15 -0700

    Update heterozygosity_run2.sh

--- RescueSimulations/heterozygosity_run2.sh
+++ RescueSimulations/heterozygosity_run2.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-
+SCRIPT=/space/s2/diana/puma/simulation/simulation_run2/SlidingWindowHet.py
 for i in {1..10}
 do   
   #Create directory for "chromosome"
   mkdir -p seed$i
-  mv *seed$i.* seed$i
+  #mv *seed$i.* seed$i
   cd seed$i
   
   #Change name of chromosome
   echo -e "1\t$i" > chromosome_mapping_file.txt
   for file in *vcf
   do
-    out=${file%.vcf.gz}_chr$i.vcf
+    bcftools index $file
+    out=${file%.vcf}_chr$i.vcf.gz
     bcftools annotate --rename-chrs chromosome_mapping_file.txt $file -Oz -o $out
     #Calculate heterozygosity
-    python3 ./SlidingWindowHet.py $out scaffolds 1000000 100000 $i &
+    python3 $SCRIPT $out scaffolds 1000000 100000 $i &
   done
-  
+  cd ..
 done
 
