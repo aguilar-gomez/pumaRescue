062285f8b29052483b523a08b64613bbd5217d8f
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2024-04-10 20:10:45 -0700

    Update rohs.sh

--- PyagourundiRef/ROHs/rohs.sh
+++ PyagourundiRef/ROHs/rohs.sh
@@ -5,8 +5,8 @@
 #$ -N ROHs
 #$ -cwd
 #$ -m bea
-#$ -o ./rohv2.out
-#$ -e ./rohv2.err
+#$ -o ./roh_unrelated.out
+#$ -e ./roh_unrelated.err
 #$ -M daguilar
 
 . /u/local/Modules/default/init/modules.sh
@@ -15,31 +15,32 @@ module load bcftools
 module load htslib
 
 VCF=puma_simplePASS_variants_all.vcf.gz 
-bcftools query -l $VCF >samples.list
+#bcftools query -l $VCF >samples.list
 
 # Identify ROH
 # -G, --GTs-only <float>             use GTs and ignore PLs, instead using <float> for PL of the two least likely genotypes.
-bcftools roh -G 30 -Orz -o ${VCF}_bcftoolsROH.txt.gz ${VCF}
+#bcftools roh -G 30 -Orz -o ${VCF}_bcftoolsROH.txt.gz ${VCF}
 
 #Repeat excluding related individuals and using only autosomes
 #KEEP=samples_keep.list # generate based on relatedness
 # Identify ROH
 # -e, --estimate-AF [TAG],<file>     estimate AF from FORMAT/TAG (GT or PL) of all samples ("-") or samples listed
-#                                            in <file>. If TAG is not given, the frequency is estimated from GT by default
-#bcftools roh -e ${KEEP} -G 30 -Orz -o ${VCF}_bcftoolsROH.txt.gz ${VCF}
+#                                        in <file>. If TAG is not given, the frequency is estimated from GT by default
+KEEP=unrelatedIndv
+bcftools roh -e ${KEEP} -G 30 -Orz -o ${VCF}_bcftoolsROH_unrelated.txt.gz ${VCF}
 
 # Reformat output
-zcat ${VCF}_bcftoolsROH.txt.gz | tail -n+4 \
+zcat ${VCF}_bcftoolsROH_unrelated.txt.gz | tail -n+4 \
 | sed 's/# //g' | sed -E 's/\[[0-9]\]//g' | sed 's/ (bp)//g' \
 | sed 's/ (average fwd-bwd phred score)//g' \
-| tr ' ' '_'> ${VCF}_bcftoolsROH.txt
+| tr ' ' '_'> ${VCF}_bcftoolsROH_unrelated.txt
 
 #Total sequence analyzed, smaller genome: 2,415,546,177
-DATA=${VCF}_bcftoolsROH.txt.gz
+DATA=${VCF}_bcftoolsROH.txt_unrelated.gz
 while read -r SAMPLE ; do 
 zcat ${DATA} \
 | awk -v s=${SAMPLE} 'BEGIN{sum=0}{if ($2==s && $6>=1e6){sum+=$6; num+=1}}END{printf "%s\t%s\t%s\t%s\n", s, sum/2415546177, num, sum/num}'
-done < samples.list > ROHS_pumas.totalgenome
+done < samples.list > ROHS_pumas_unrelated.totalgenome
 
 
 
@@ -69,14 +70,16 @@ done < samples.list
 
 ################################################################################################################
 ### plink
+qrsh -l h_rt=4:00:00,h_data=40G -pe shared 2
+
 module load plink
 # Convert simplified, concatenated VCF file to plink format
-FILE=bricei_order
-plink --vcf bricei_simplePASS_variants_all.vcf.gz --make-bed --out $FILE --allow-extra-chr --keep-allele-order --double-id 
+FILE=pumas
+plink --vcf puma_simplePASS_variants_all.vcf.gz --make-bed --out $FILE --allow-extra-chr --keep-allele-order --double-id 
 
 # Identify ROH and reformat output
 plink --bfile ${FILE} --out ${FILE}_ROH --homozyg --allow-extra-chr 
-#--homozyg: Scan complete, found 5214 ROH.
+#--homozyg: Scan complete, found 10481 ROH.
 sed -i -E 's/^\ +//g' ${FILE}_ROH.hom
 sed -i -E 's/\ +/\t/g' ${FILE}_ROH.hom 
 
