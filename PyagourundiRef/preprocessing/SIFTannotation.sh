765ec556a9b5436315408700382f697170cd30b6
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2024-04-24 12:52:25 -0700

    Create SIFTannotation.sh

---
+++ PyagourundiRef/preprocessing/SIFTannotation.sh
@@ -0,0 +1,5 @@
+
+git clone --recursive https://github.com/rvaser/sift4g.git sift4
+git clone https://github.com/pauline-ng/SIFT4G_Create_Genomic_DB.git scripts_to_build_SIFT_db
+cd test_files/
+cp homo_sapiens-test.txt puma_concolor.txt
