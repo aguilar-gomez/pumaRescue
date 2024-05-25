44c44422429deb83fa570360a8890d5ab79fcae7
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2024-05-25 00:43:14 -0700

    Update and rename extractVCFs.sh to extractSNPs.sh

---
+++ PyagourundiRef/DeleteriousVariation/extractSNPs.sh
@@ -0,0 +1,26 @@
+### FilterVCF
+#!/bin/bash
+#$ -cwd
+#$ -j y
+#$ -o simplify.log.$JOB_ID.$TASK_ID
+#$ -l highp,h_rt=24:00:00,h_data=24G
+## and the number of cores as needed:
+#$ -pe shared 1
+#$ -M daguilar
+#$ -t 1-237:1
+
+. /u/local/Modules/default/init/modules.sh
+module load bcftools
+module load htslib
+
+### Simplify & exclude sites failing filters, extract SNPs 
+IDX=$(printf %03d ${SGE_TASK_ID})
+export VCF=puma_${IDX}simple_PASS_SIFT.vcf.gz 
+
+zcat $VCF\
+| awk '{ if ( $0~/^#/ || $8~"VariantType=SNP" ){print $0}}' \
+| bgzip > puma_${IDX}_simple_SIFT_SNPs.vcf.gz
+
+tabix -p vcf puma_${IDX}_simple_SIFT_SNPs.vcf.gz
+
+
