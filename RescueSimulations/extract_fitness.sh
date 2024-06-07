356c91f00d969efe2088c46bbdec495907d787ec
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2024-06-07 00:33:30 -0700

    Update extract_fitness.sh

--- RescueSimulations/extract_fitness.sh
+++ RescueSimulations/extract_fitness.sh
@@ -1,4 +1,4 @@
-
+#Previous format
 for file in c*_noGR_*out; do echo $file; tail -11850 $file>${file%%.out}_fitness.out ; done
 
 # Define the list of line numbers to search for
@@ -27,4 +27,36 @@ awk '{
   average = sum / NF;
   print average;
 }' combined_fitness_values_noGR > row_averages_noGR
+
+
+#Current format
+for file in cfp_tx*out
+do
+grep "^43900\|^43911\|^43916\|^43921\|^43961\|^44011\|^44410" $file > $file.lines
+done
+
+paste -d " " cfp_tx_GR_*lines|cut -d " " -f2,5,8,11,14,17,20,23,26,29 > GR_TX_10runs
+paste -d " " cfp_tx_noGR_*lines|cut -d " " -f2,5,8,11,14,17,20,23,26,29 > noGR_TX_10runs
+
+paste -d " " cfp_tx_GR_*lines|cut -d " " -f3,6,9,12,15,18,21,24,27,30 > GR_FL_10runs
+paste -d " " cfp_tx_noGR_*lines|cut -d " " -f3,6,9,12,15,18,21,24,27,30 > noGR_FL_10runs
+
+awk '
+{
+    sum = 0
+    for (i = 1; i <= NF; i++) {
+        sum += $i
+    }
+    if (NF > 0) {
+        average = sum / NF
+        printf " %.3f\n", average
+    }
+}
+' noGR_FL_10runs > averagenoGRFL
+GR_FL_10runs > averageGRFL
+GR_TX_10runs > averageGRTX
+noGR_TX_10runs > averagenoGRTX
+
+
+
 
