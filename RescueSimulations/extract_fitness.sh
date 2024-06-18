a7b92968f67eb1c0f2690f42d7efd7682100b704
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2024-06-18 14:29:12 -0700

    Update extract_fitness.sh

--- RescueSimulations/extract_fitness.sh
+++ RescueSimulations/extract_fitness.sh
@@ -28,7 +28,7 @@ awk '{
   print average;
 }' combined_fitness_values_noGR > row_averages_noGR
 
-
+################################################################################################################################
 #Current format
 for file in cfp_tx*out
 do
@@ -46,8 +46,8 @@ paste -d " " cfp_tx_noGR_*lines|cut -d " " -f3,6,9,12,15,18,21,24,27,30 > noGR_F
 paste -d " " cfp_tx_GR_*lines|cut -d " " -f2,5,8,11,14,17,20,23,26,29,32,35,38,41,44,47,50,53,56,59 > GR_TX_20runs
 paste -d " " cfp_tx_noGR_*lines|cut -d " " -f2,5,8,11,14,17,20,23,26,29,32,35,38,41,44,47,50,53,56,59  > noGR_TX_20runs
 
-paste -d " " cfp_tx_GR_*lines|cut -d " " -f3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60 > GR_FL_10runs
-paste -d " " cfp_tx_noGR_*lines|cut -d " " -f3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60 > noGR_FL_10runs
+paste -d " " cfp_tx_GR_*lines|cut -d " " -f3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60 > GR_FL_20runs
+paste -d " " cfp_tx_noGR_*lines|cut -d " " -f3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60 > noGR_FL_20runs
 
 awk '
 {
@@ -60,12 +60,12 @@ awk '
         printf " %.3f\n", average
     }
 }
-' GR_FL_10runs > averageGRFL
+'  noGR_TX_20runs > averagenoGRTX
 
 
-noGR_TX_20runs > averagenoGRTX
+noGR_FL_20runs > averagenoGRFL
+GR_FL_20runs > averageGRFL
 GR_TX_20runs > averageGRTX
- noGR_FL_10runs > averagenoGRFL
 
 
 
@@ -76,4 +76,6 @@ grep "^15" $file > $file.fitness
 done
 
 
+paste -d " " cfp_tx_GR_*fitness|cut -d " " -f3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60 > allfitnessGR
+paste -d " " cfp_tx_noGR_*fitness|cut -d " " -f3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60 > allFitnessnoGR
 
