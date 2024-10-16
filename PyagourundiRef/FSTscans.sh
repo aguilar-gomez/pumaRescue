3c3f1021f73de67d8ad33d218df7a1fc4fd8e6ea
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2024-10-16 14:37:11 -0700

    Update FSTscans.sh

--- PyagourundiRef/FSTscans.sh
+++ PyagourundiRef/FSTscans.sh
@@ -7,22 +7,17 @@ ls /space/s2/diana/puma/allbams_Pyaguarundi/bam_output_allsampletoOutgroup/TX*ba
 bcftools query -f '%CHROM\t%POS\t%REF\t%ALT\n' puma_simplePASS_variants_all.vcf.gz  > puma_variants
 bcftools query -f '%CHROM\t%POS\t%REF\t%ALT\n' puma_simplePASS_all.vcf.gz  > puma_sites
 SITES=puma_sites
+RF= 
 angsd index sites $SITES
+
+
 #Create Saf files
 #No SNP filtering, keep invariable
-bamlist=$1
-REF=
-angsd -bam $bamlist -out $SITES -minMapQ 25 -minQ 25 -remove_bads 1 -uniqueOnly 1 -only_proper_pairs 1 -GL 1 -doMaf 1 -doMajorMinor 4 -doGlf 2 -nThreads 10 -ref $REF -setMaxDepthInd 50 -doCounts 1 -skipTriallelic 1
-
-#SNP filtering
-#Do all filters and generate beagle simultaneously
-/home/diana/bin/angsd -bam $bamlist -out pumilio_version11 -minInd 250 -setMinDepthInd 1 \ 
-                      -minMapQ 25 -minQ 25 -remove_bads 1 -uniqueOnly 1 -only_proper_pairs 1 \
-                      -GL 1 -doMaf 1 -doMajorMinor 4 -doGlf 2 -SNP_pval 1e-6 -nThreads 20 \
-                      -ref $REF -setMaxDepthInd 50 -doCounts 1 -skipTriallelic 1 -dosnpstat 1 \
-                      -doHWE 1 -sb_pval 1e-4 -hetbias_pval 1e-6 -doGeno 3 -sites pumilio_sites_all.pos \
-                      -rf pumilio_sites_all.rf -doBcf 1 -doPost 1 --ignore-RG 0 -edge_pval 1e-4 \
-                      -mapQ_pval 1e-4
+POP=$1
+bamlist=${POP}bamlist
+REF=/space/s1/lin.yuan/puma/genome_outgroup/GCF_014898765.1_PumYag_genomic.fna
+angsd -bam $bamlist -out $POP -sites $SITES -rf $RF -minMapQ 25 -minQ 25 -remove_bads 1 \
+      -uniqueOnly 1 -only_proper_pairs 1 -GL 1 -nThreads 10 -ref $REF -doSaf 1 
 
 
 
