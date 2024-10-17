0c33471de42ff2197535eabde8b19703e27184cf
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2024-10-16 18:46:42 -0700

    Update FSTscans.sh

--- PyagourundiRef/FSTscans.sh
+++ PyagourundiRef/FSTscans.sh
@@ -9,7 +9,7 @@ bcftools query -f '%CHROM\t%POS\t%REF\t%ALT\n' puma_simplePASS_all.vcf.gz  > pum
 cut -f1-2 puma_sites > puma_sites.txt
 angsd sites index puma_sites.txt
 cut -f1 puma_sites.txt|sort|uniq > puma_sites.rf
-
+sort -k1 puma_sites.txt > pyag_sites.txt
 #Create Saf files
 #No SNP filtering, keep invariable
 #!/bin/bash
@@ -23,14 +23,17 @@ angsd -bam $bamlist -out $POP -sites $SITES -rf $RF -minMapQ 30 -minQ 25 -remove
 
 #Run for populations
 nohup sh doSaf.sh CFP > out.CFP &
+nohup sh doSaf.sh PTFP > out.PTFP &
+nohup sh doSaf.sh TX > out.TX &
 
 #Fst index and scan use unfolded saf
+#!/bin/bash
 pop1=$1
 pop2=$2
 #joint SFS
-realSFS $pop1.r.saf.idx $pop2.r.saf.idx -P 5 >$pop1.$pop2.ml
+realSFS $pop1.saf.idx $pop2.saf.idx -P 5 >$pop1.$pop2.ml
 #Fst index
-realSFS fst index $pop1.r.saf.idx $pop2.r.saf.idx -sfs $pop1.$pop2.ml  -fstout $pop1.$pop2
+realSFS fst index $pop1.saf.idx $pop2.saf.idx -sfs $pop1.$pop2.ml  -fstout $pop1.$pop2
 realSFS fst stats $pop1.$pop2.fst.idx > fstindex$pop1$pop2
 #Fst scan
 realSFS fst stats2 $pop1.$pop2.fst.idx -win 100000 -step 20000 -type 2 > fst_w100kb$pop1$pop2
@@ -42,12 +45,9 @@ pop1=$1
 pop2=$2
 pop3=$3
 SNP=$4
-realSFS fst index $pop1.r.saf.idx $pop2.r.saf.idx $pop3.r.saf.idx -sfs $pop1.$pop2.ml -sfs $pop1.$pop3.ml -sfs $pop2.$pop3.ml -fstout $pop1.$pop2.$pop3
+realSFS fst index $pop1.saf.idx $pop2.saf.idx $pop3.saf.idx -sfs $pop1.$pop2.ml -sfs $pop1.$pop3.ml -sfs $pop2.$pop3.ml -fstout $pop1.$pop2.$pop3
 realSFS fst stats  $pop1.$pop2.$pop3.fst.idx>pbs.$pop1.$pop2.$pop3
 realSFS fst stats2 $pop1.$pop2.$pop3.fst.idx -win 100000 -step 20000 -type 2 >slidingwindow100Ks20k.$pop1.$pop2.$pop3
-#Make SNP windows
-realSFS fst print $pop1.$pop2.$pop3.fst.idx  > $pop1.$pop2.$pop3.printed.txt
-PBS_SNP_windows.py $pop1.$pop2.$pop3.printed.txt $pop1.$pop2.$pop3.wSNP$SNP $SNP
 
 #Tajimas D and thetas 
 #The values of the different thetas are negative because they are logscale!
@@ -56,13 +56,4 @@ PBS_SNP_windows.py $pop1.$pop2.$pop3.printed.txt $pop1.$pop2.$pop3.wSNP$SNP $SNP
 realSFS saf2theta $pop.saf.idx -sfs $pop.folded.sfs -outname $pop
 thetaStat print $pop.thetas.idx > $pop.thetas.persite.txt
 
-#For Dxy
-nohup realSFS -P 4 $POP.r.saf.idx -sites $pop1.$pop2.shared.pos -fold 1 1> Results/$POP.sfs 2>Results/$POP.outsfs &
 
-#Calculate Dxy
-$NGSTOOLS/ngsStat -npop 2 -postfiles $pop1.shared.saf $pop2.shared.saf -nsites $NSITES -nind $pop1_nind $pop2_nind -outfile $pop1.$pop2.stats.txt
-
-
-#Do windows of SNPS
-Tajimas_SNPmidpoint.py $pop.thetas.persite.shared.sorted $pop.Tajimas.midpoint.SNP$sites $sites $popsize &
-SNPwindows_midpoint.py $pop1.$pop2.persite.Dxy $pop1.$pop2.SNP200.Dxy.midpoint 200 Dxy &
