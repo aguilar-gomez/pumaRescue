c907ff76508801f634109758d0a6a5aba15de6a7
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2024-10-24 15:10:53 -0700

    Update extract_fitness.sh

--- RescueSimulations/extract_fitness.sh
+++ RescueSimulations/extract_fitness.sh
@@ -1,34 +1,3 @@
-#Previous format
-for file in c*_noGR_*out; do echo $file; tail -11850 $file>${file%%.out}_fitness.out ; done
-
-# Define the list of line numbers to search for
-line_numbers=("43900" "43911" "43916" "43921" "43961" "44011" "44410")
-
-for file in *fitness*; do
-  for number in "${line_numbers[@]}"; do
-    linenumber=$(grep -n "$number" "$file" | cut -d: -f1)
-    if [ -n "$linenumber" ]; then
-      endline=$((linenumber + 2))
-      # Extract lines and filter only floating-point numbers
-      sed -n "${linenumber},${endline}p" "$file" | grep -o '[0-9]*\.[0-9]*' >> "fitness_extracted_$file"
-    fi
-  done
-done
-
-
-paste fitness_extracted_cfp_tx_noGR_* > combined_fitness_values_noGR
-
-# Calculate the average per row
-awk '{
-  sum = 0;
-  for (i = 1; i <= NF; i++) {
-    sum += $i;
-  }
-  average = sum / NF;
-  print average;
-}' combined_fitness_values_noGR > row_averages_noGR
-
-################################################################################################################################
 #Current format
 for file in cfp_tx*out
 do
@@ -79,3 +48,35 @@ done
 paste -d " " cfp_tx_GR_*fitness|cut -d " " -f3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60 > allfitnessGR
 paste -d " " cfp_tx_noGR_*fitness|cut -d " " -f3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60 > allFitnessnoGR
 
+
+################################################################################################################################
+
+#Previous format
+for file in c*_noGR_*out; do echo $file; tail -11850 $file>${file%%.out}_fitness.out ; done
+
+# Define the list of line numbers to search for
+line_numbers=("43900" "43911" "43916" "43921" "43961" "44011" "44410")
+
+for file in *fitness*; do
+  for number in "${line_numbers[@]}"; do
+    linenumber=$(grep -n "$number" "$file" | cut -d: -f1)
+    if [ -n "$linenumber" ]; then
+      endline=$((linenumber + 2))
+      # Extract lines and filter only floating-point numbers
+      sed -n "${linenumber},${endline}p" "$file" | grep -o '[0-9]*\.[0-9]*' >> "fitness_extracted_$file"
+    fi
+  done
+done
+
+
+paste fitness_extracted_cfp_tx_noGR_* > combined_fitness_values_noGR
+
+# Calculate the average per row
+awk '{
+  sum = 0;
+  for (i = 1; i <= NF; i++) {
+    sum += $i;
+  }
+  average = sum / NF;
+  print average;
+}' combined_fitness_values_noGR > row_averages_noGR
