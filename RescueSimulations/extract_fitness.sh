335a1ae3c0c8a00dac71b1600ea90d0e4bd1c9ab
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2024-10-24 16:00:19 -0700

    Update extract_fitness.sh

--- RescueSimulations/extract_fitness.sh
+++ RescueSimulations/extract_fitness.sh
@@ -11,12 +11,12 @@ paste -d " " cfp_tx_noGR_*lines|cut -d " " -f2,5,8,11,14,17,20,23,26,29 > noGR_T
 paste -d " " cfp_tx_GR_*lines|cut -d " " -f3,6,9,12,15,18,21,24,27,30 > GR_FL_10runs
 paste -d " " cfp_tx_noGR_*lines|cut -d " " -f3,6,9,12,15,18,21,24,27,30 > noGR_FL_10runs
 
-
-paste -d " " cfp_tx_GR_*lines|cut -d " " -f2,5,8,11,14,17,20,23,26,29,32,35,38,41,44,47,50,53,56,59 > GR_TX_20runs
-paste -d " " cfp_tx_noGR_*lines|cut -d " " -f2,5,8,11,14,17,20,23,26,29,32,35,38,41,44,47,50,53,56,59  > noGR_TX_20runs
-
-paste -d " " cfp_tx_GR_*lines|cut -d " " -f3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60 > GR_FL_20runs
-paste -d " " cfp_tx_noGR_*lines|cut -d " " -f3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60 > noGR_FL_20runs
+#Fitnes in Texas
+paste -d " " cfp_tx*_GR_*lines|cut -d " " -f2,5,8,11,14,17,20,23,26,29,32,35,38,41,44,47,50,53,56,59 > GR_TX_20runs
+paste -d " " cfp_tx*_noGR_*lines|cut -d " " -f2,5,8,11,14,17,20,23,26,29,32,35,38,41,44,47,50,53,56,59  > noGR_TX_20runs
+#Fitnes in Florida
+paste -d " " cfp_tx*_GR_*lines|cut -d " " -f3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60 > GR_FL_20runs
+paste -d " " cfp_tx*_noGR_*lines|cut -d " " -f3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60 > noGR_FL_20runs
 
 awk '
 {
@@ -29,12 +29,10 @@ awk '
         printf " %.3f\n", average
     }
 }
-'  noGR_TX_20runs > averagenoGRTX
-
-
-noGR_FL_20runs > averagenoGRFL
+'  GR_TX_20runs > averageGRTX
 GR_FL_20runs > averageGRFL
-GR_TX_20runs > averageGRTX
+noGR_FL_20runs > averagenoGRFL
+noGR_TX_20runs > averagenoGRTX
 
 
 
