5170c1bf6674c8c0e0fde600cf43fb79e941b22a
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2024-10-25 12:56:05 -0700

    Update extract_fitness.sh

--- RescueSimulations/extract_fitness.sh
+++ RescueSimulations/extract_fitness.sh
@@ -9,6 +9,7 @@ paste -d " " cfp_tx*_GR_*fitness|cut -d " " -f3,6,9,12,15,18,21,24,27,30,33,36,3
 paste -d " " cfp_tx*_noGR_*fitness|cut -d " " -f3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60 > allFitnessnoGR
 
 
+cut -f1 -d " " cfp_tx_recentsplit_noGR_3.out.fitness > times_recentsplit
 
 
 #Current format
